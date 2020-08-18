e1cdff847df6f4658d8895956a66de2bbd1cc071
Abdul Razzak <mohammedabdul@gmail.com>
2020-08-18 16:23:48 +0530

    Updates

---
+++ precache-manifest.a8f31ef3da9521d157379c4e12ffefcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdaf64d9f5471058b60087050c18d91",
+    "url": "/smart_dropdown/index.html"
+  },
+  {
+    "revision": "928bda7c734439cc1db7",
+    "url": "/smart_dropdown/static/css/2.95b5b846.chunk.css"
+  },
+  {
+    "revision": "8ffc89eae4e3f7451808",
+    "url": "/smart_dropdown/static/css/main.079ba791.chunk.css"
+  },
+  {
+    "revision": "928bda7c734439cc1db7",
+    "url": "/smart_dropdown/static/js/2.a8caa98c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/smart_dropdown/static/js/2.a8caa98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ffc89eae4e3f7451808",
+    "url": "/smart_dropdown/static/js/main.e6b94cd7.chunk.js"
+  },
+  {
+    "revision": "bc82481b55cc4458e5f6",
+    "url": "/smart_dropdown/static/js/runtime-main.12ea2170.js"
+  }
+]);
